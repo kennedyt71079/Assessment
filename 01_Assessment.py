747344bbb31feb57e816bddaae3f39640efd9184
kennedyt71079 <58008618+kennedyt71079@users.noreply.github.com>
2020-07-21 16:19:48 +1200

    CODING EXPERT

--- 01_Assessment.py
+++ 01_Assessment.py
@@ -37,23 +37,51 @@ def intcheck(question, low=None, high=None):
             print(error)
             continue
 
+# Welcome Text
+print("Welcome to the addition quiz!!! :)")
+print()
+print("The game generates 2 random numbers for each question,")
+print("The low number is how low the number is generated,")
+print("The High number is how high the number is generated,")
+print("And the questions are how many equations you will be asked!")
+print("Please answer the questions below to start the game :)")
+print()
+
 # Main Routine
 low = intcheck("Please choose a low number - ")
 high = intcheck("Please choose a high number - ", low + 1)
 questions = intcheck("How many questions do you want to play? ")
+print("Good Luck, Have Fun!")
+questions_answered = 0
+right = 0
+wrong = 0
 
-random1 = random.randint(low, high)
-random2 = random.randint(low, high)
-answer = random1+random2
+# Generates random numbers, adds them together, and loops from here
+while questions_answered < questions:
+    random1 = random.randint(low, high)
+    random2 = random.randint(low, high)
+    answer = random1+random2
 
-# Generates the Equation
-guess = intcheck("What is {} + {} = ".format(random1, random2))
+    # Generates the Equation
+    print()
+    guess = intcheck("What is {} + {} = ".format(random1, random2))
 
-# Checks to see if the answer is correct
-if guess == random1 + random2:
-    print("That is correct!")
-else:
-    print("That is incorrect, The answer was {}".format(answer))
+    # Checks to see if the answer is correct
+    if guess == random1 + random2:
+        print("Congratulations, That is correct!")
+        right += 1
+        print("You have gotten {} Correct and {} Wrong".format(right, wrong))
+    else:
+        print("Sorry, That is incorrect, The answer was {}".format(answer))
+        wrong += 1
+        print("You have gotten {} Correct and {} Wrong".format(right, wrong))
+    questions_answered += 1
 
-print(random1)
-print(random2)
+    # Checks to see how many questions are left
+    if questions_answered == questions:
+        continue
+
+# End game text
+print()
+print("You have gotten to the end of the quiz")
+print("You got {} out of {} Questions right!".format(right, questions))
